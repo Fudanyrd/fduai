126372e1ee9fab824d8edb5cb73b5a323954f87a
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2025-05-23 02:30:36 +0000

    [IMPL] autograd for matmul, sub, mul operators

---
+++ autograd/__init__.py
@@ -0,0 +1,3 @@
+from tensor import Tensor, Device
+from .node import DataNode
+from .op import Operator
--- autograd/node.py
+++ autograd/node.py
@@ -1,5 +1,5 @@
-from tensor_module import Tensor, Device
-from op import Operator
+from tensor import Tensor, Device
+from .op import Operator
 
 class DataNode():
     topological_order = []
@@ -21,6 +21,46 @@ class DataNode():
 
         ret.op = Operator.ADD
         ret.inputs = [self, other]
+        ret.requires_grad = self.requires_grad or other.requires_grad
+
+        return ret
+    
+    def __sub__(self, other):
+        t = self.tensor - other.tensor
+        ret = DataNode(t, requires_grad=False)
+
+        ret.op = Operator.SUB
+        ret.inputs = [self, other]
+        ret.requires_grad = self.requires_grad or other.requires_grad
+
+        return ret
+    
+    def __mul__(self, other):
+        t = self.tensor * other.tensor
+        ret = DataNode(t, requires_grad=False)
+
+        ret.op = Operator.MUL
+        ret.inputs = [self, other]
+        ret.requires_grad = self.requires_grad or other.requires_grad
+
+        return ret
+
+    def __neg__(self):
+        ret = DataNode(-self.tensor, requires_grad=self.requires_grad)
+
+        ret.op = Operator.NEG
+        ret.inputs = [self]
+
+        return ret
+    
+    @staticmethod 
+    def matmul(lh, rh):
+        t = Tensor.dot(lh.tensor, rh.tensor)
+        ret = DataNode(t, requires_grad=False)
+
+        ret.op = Operator.MATMUL
+        ret.inputs = [lh, rh]
+        ret.requires_grad = lh.requires_grad or rh.requires_grad
 
         return ret
 
@@ -30,6 +70,8 @@ class DataNode():
             node.grad = None
 
     def _add_grad(self, grad):
+        if grad.shape != self.tensor.shape:
+            grad = Tensor.grad_reshape(grad, self.shape())
         if self.grad is None:
             self.grad = grad
         else:
@@ -51,6 +93,43 @@ class DataNode():
             else:
                 lh.backward(lh.grad)
                 rh.backward(rh.grad)
+        elif self.op == Operator.SUB:
+            self.inputs[0]._add_grad(grad)
+            self.inputs[1]._add_grad(-grad)
+
+            lh, rh = self.inputs[0], self.inputs[1]
+            if DataNode.topological_order.index(lh) < DataNode.topological_order.index(rh):
+                rh.backward(rh.grad)
+                lh.backward(lh.grad)
+            else:
+                lh.backward(lh.grad)
+                rh.backward(rh.grad)
+        elif self.op == Operator.MUL:
+            self.inputs[0]._add_grad(grad * self.inputs[1].tensor)
+            self.inputs[1]._add_grad(grad * self.inputs[0].tensor)
+
+            lh, rh = self.inputs[0], self.inputs[1]
+            if DataNode.topological_order.index(lh) < DataNode.topological_order.index(rh):
+                rh.backward(rh.grad)
+                lh.backward(lh.grad)
+            else:
+                lh.backward(lh.grad)
+                rh.backward(rh.grad)
+        elif self.op == Operator.MATMUL:
+            self.inputs[0]._add_grad(Tensor.dot(grad, Tensor.transpose(self.inputs[1].tensor)))
+            self.inputs[1]._add_grad(Tensor.dot(Tensor.transpose(self.inputs[0].tensor), grad))
+
+            lh, rh = self.inputs[0], self.inputs[1]
+            if DataNode.topological_order.index(lh) < DataNode.topological_order.index(rh):
+                rh.backward(rh.grad)
+                lh.backward(lh.grad)
+            else:
+                lh.backward(lh.grad)
+                rh.backward(rh.grad)
+        elif self.op == Operator.NEG:
+            src = self.inputs[0]
+            src._add_grad(-grad)
+            src.backward(src.grad)
         else:
             raise NotImplementedError()
 
@@ -80,4 +159,15 @@ if __name__ == '__main__':
     # should print None, [1], None
     a.backward()
     print(c.grad, a.grad.to_list(), b.grad)
+    DataNode.zero_grad()
+
+    x = a - b
+    x.backward()
+    print(a.grad.to_list(), b.grad.to_list())
+    DataNode.zero_grad()
+
+    x = a - b 
+    y = x * x
+    y.backward()
+    print(a.grad.to_list(), b.grad.to_list())
     DataNode.zero_grad()
